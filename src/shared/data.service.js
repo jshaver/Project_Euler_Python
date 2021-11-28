55edc9f8d3f8d4d0b0b9ab0decef07166fa4fd57
jshaver <johnshaver@protonmail.com>
2021-11-28 15:46:10 -0800

    Fixed minor route error, pulling from json db

--- src/shared/data.service.js
+++ src/shared/data.service.js
@@ -7,7 +7,8 @@ const getProblems = async function(){
   try{
     const response = await axios.get(`${API}/problems`);
     let data = parseList(response);
-    logger.info('getProblems success', data);
+    //logger.info('getProblems success', data);
+    logger.info('getProblems success');
     return data;
   } catch(e) {
     logger.error(e);
@@ -19,7 +20,8 @@ const getSolutions = async function(){
   try{
     const response = await axios.get(`${API}/solutions`);
     let data = parseList(response);
-    logger.info('getSolutions success', data);
+    //logger.info('getSolutions success', data);
+    logger.info('getSolutions success');
     return data;
   } catch(e) {
     logger.error(e);
